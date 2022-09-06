2b7cec424e55de384949613524da6b852b44daa5
kang jin wook <wlsdnr1111@naver.com>
2022-09-06 13:56:39 +0900

    initial commit

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testJoin() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("Kang");
+        
+        //when
+        Long saveId = memberService.join(member);
+        
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+    
+    @Test()
+    public void testExceptionJoin() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kang");
+
+        Member member2 = new Member();
+        member2.setName("kang");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
+}
